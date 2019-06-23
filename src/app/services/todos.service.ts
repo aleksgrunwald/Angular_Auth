3eacef58a68df7304ba62ffb15ba518b96e4fa8a
Aleksandra <aleks.grunwald@gmail.com>
2019-06-23 22:06:50 +0200

    Proper rxjs import

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Todo } from '../interfaces/todo';
-import { Observable } from 'rxjs/Observable';
+import {Observable} from "rxjs";
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class TodosService {
   constructor(private http: HttpClient) { }
 
   getTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>(this._url:);
+    return this.http.get<Todo[]>(this._url);
   }
 
 }
