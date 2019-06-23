aee269ef4a6adbbd84e2d219f44f853af8e068dc
Aleksandra <aleks.grunwald@gmail.com>
2019-06-23 23:40:21 +0200

    Mongoose init

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
+const path = require('path');
 
 const PORT = 3000;
+const api = require('./routes/api')
 
 const app = express();
 
 app.use(bodyParser.json());
+// app.use(favicon(__dirname + '/angularFront/favicon.ico'));
 
+app.use('/api', api);
 app.listen(PORT, () => console.log('Server running on localhost: ' + PORT));
 
 app.get('/', (req, res) => res.send('Hello from server'));
